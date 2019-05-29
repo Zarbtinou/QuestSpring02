d18ab03b38379e8e7f5aa3f7b13e9e2a2dcefd2a
Quentin Moreau <qdr.moreau@yahoo.fr>
2019-05-29 11:14:05 +0200

    Add files for the quest

---
+++ src/main/java/com/wildcodeschool/myProjectWithDB/QuestSpring02Application.java
@@ -0,0 +1,15 @@
+package com.wildcodeschool.myProjectWithDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestSpring02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestSpring02Application.class, args);
+	}
+
+}
+
+
