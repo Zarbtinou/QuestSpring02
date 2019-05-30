34c52d36cac73e04cee30d5b9865f330afbfefd9
Quentin Moreau <qdr.moreau@yahoo.fr>
2019-05-30 19:04:15 +0200

    Add files for the INSERT quest

---
+++ src/main/java/com/wildcodeschool/myProjectWithDB/repositories/SchoolRepository.java
@@ -0,0 +1,98 @@
+package com.wildcodeschool.myProjectWithDB.repositories;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.wildcodeschool.myProjectWithDB.entities.School;
+
+public class SchoolRepository {
+
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/wild_db_quest?serverTimezone=GMT";
+    private final static String DB_USER = "___";//Replace ___ by ur user
+    private final static String DB_PASSWORD = "___";//Replace ___ by ur password
+
+
+    public static School selectById(int id) {
+        try(
+                Connection connection = DriverManager.getConnection(
+                        DB_URL, DB_USER, DB_PASSWORD
+                );
+                PreparedStatement statement = connection.prepareStatement(
+                        "SELECT * FROM school WHERE id=?"
+                );
+        ) {
+            statement.setInt(1, id);
+
+            try(
+                    ResultSet resulSet = statement.executeQuery();
+            ) {
+                if(resulSet.next()){
+                    String name = resulSet.getString("name");
+                    int capacity = resulSet.getInt("capacity");
+                    String country = resulSet.getString("country");
+
+                    return new School(id, name, capacity, country);
+                }
+                else {
+                    throw new ResponseStatusException(
+                            HttpStatus.INTERNAL_SERVER_ERROR, "unknown id in table school"
+                    );
+                }
+            }
+        }
+        catch (SQLException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "", e
+            );
+        }
+    }
+
+    public static int insert(
+            String name,
+            int capacity,
+            String country
+    ) {
+        try(
+                Connection connection = DriverManager.getConnection(
+                        DB_URL, DB_USER, DB_PASSWORD
+                );
+                PreparedStatement statement = connection.prepareStatement(
+                        "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS
+                );
+        ) {
+            statement.setString(1, name);
+            statement.setInt(2, capacity);
+            statement.setString(3, country);
+
+            if(statement.executeUpdate() != 1) {
+                throw new ResponseStatusException(
+                        HttpStatus.INTERNAL_SERVER_ERROR, "failed to insert data"
+                );
+            }
+
+            try(
+                    ResultSet generatedKeys = statement.getGeneratedKeys();
+            ) {
+                if(generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                }
+                else {
+                    throw new ResponseStatusException(
+                            HttpStatus.INTERNAL_SERVER_ERROR, "failed to get inserted id"
+                    );
+                }
+            }
+        }
+        catch (SQLException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "", e
+            );
+        }
+    }
+
+}
