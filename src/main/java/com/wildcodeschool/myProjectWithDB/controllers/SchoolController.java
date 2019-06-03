1018cada23be44acdd288ed7f96631f4f0cac9dc
Quentin Moreau <qdr.moreau@yahoo.fr>
2019-06-03 10:17:31 +0200

    Add files for quest 4

--- src/main/java/com/wildcodeschool/myProjectWithDB/controllers/SchoolController.java
+++ src/main/java/com/wildcodeschool/myProjectWithDB/controllers/SchoolController.java
@@ -32,6 +32,23 @@ public class SchoolController {
                 idGeneratedByInsertion
         );
     }
+
+    @PutMapping("/api/schools/{id}")
+    public School update(
+            @PathVariable int id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer capacity,
+            @RequestParam(required = false) String country
+    ) {
+        School school = SchoolRepository.selectById(id);
+        SchoolRepository.update(
+                id,
+                name != null ? name : school.getName(),
+                capacity != null ? capacity : school.getCapacity(),
+                country != null ? country : school.getCountry()
+        );
+        return SchoolRepository.selectById(id);
+    }
 }
 
 
